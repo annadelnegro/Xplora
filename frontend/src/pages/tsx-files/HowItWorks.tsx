c880700d06e600a4258e0d50a39dd718d9f89030
annadelnegro <annaluciadelnegro@gmail.com>
2024-11-28 14:19:36 -0500

    how it works fixes

--- frontend/src/pages/tsx-files/HowItWorks.tsx
+++ frontend/src/pages/tsx-files/HowItWorks.tsx
@@ -4,9 +4,9 @@ import logo from '../../images/logo.png';
 import { Link } from 'react-router-dom';
 
 import signup from '../../images/signup.png';
-import explore from '../../images/explore.png';
 import customize from '../../images/customize.png';
-import connect from '../../images/connect.png';
+import plane from '../../images/xplora-plane.png';
+import ready from '../../images/ready.png';
 
 const HowItWorksPage: React.FC = () => {
   return (
@@ -33,25 +33,25 @@ const HowItWorksPage: React.FC = () => {
           <div className="step">
             <h2 style={{ color: '#2E8B57' }}>Step 1: Sign Up</h2>
             <img src={signup} alt="Signup Logo" id="signup-logo-howitworks" />
-            <p>Register an account with Xplora to access personalized features.</p>
+            <p>Get started with XPLORA and take the stress out of travel—sign up now to effortlessly organize your trips.</p>
           </div>
 
           <div className="step">
-            <h2 style={{ color: '#6666FF' }}>Step 2: Explore</h2>
-            <img src={explore} alt="Explore Logo" id="explore-logo-howitworks" />
-            <p>Browse and explore the features available to you on the platform.</p>
+            <h2 style={{ color: '#6666FF' }}>Step 2: Add Trips</h2>
+            <img src={plane} alt="Explore Logo" id="explore-logo-howitworks" />
+            <p>Add your upcoming flights, stays, and activities to your profile and save time for what truly matters—enjoying the journey.</p>
           </div>
 
           <div className="step">
             <h2 style={{ color: '#FF9933' }}>Step 3: Customize</h2>
             <img src={customize} alt="Customize Logo" id="customize-logo-howitworks" />
-            <p>Personalize your experience by updating your settings and preferences.</p>
+            <p>Tailor your trips your way—add photos, notes, and all the details that matter most.</p>
           </div>
 
           <div className="step">
-            <h2 style={{ color: '#009999' }}>Step 4: Connect</h2>
-            <img src={connect} alt="Connect Logo" id="connect-logo-howitworks" />
-            <p>Engage with the community and travel away.</p>
+            <h2 style={{ color: '#009999' }}>Step 4: Travel Away!</h2>
+            <img src={ready} alt="Connect Logo" id="connect-logo-howitworks" />
+            <p>We are now ready to XPLORA place together!</p>
           </div>
         </section>
       </main>
