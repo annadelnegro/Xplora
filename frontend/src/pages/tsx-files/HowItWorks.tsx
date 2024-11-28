2fbedc1d963cda7ad3a0e5864a45cb3f8465d158
annadelnegro <annaluciadelnegro@gmail.com>
2024-11-28 13:59:59 -0500

    how it works page small fixes

--- frontend/src/pages/tsx-files/HowItWorks.tsx
+++ frontend/src/pages/tsx-files/HowItWorks.tsx
@@ -3,6 +3,11 @@ import '../css-files/HowItWorks.css'; // Create this CSS file for styling
 import logo from '../../images/logo.png';
 import { Link } from 'react-router-dom';
 
+import signup from '../../images/signup.png';
+import explore from '../../images/explore.png';
+import customize from '../../images/customize.png';
+import connect from '../../images/connect.png';
+
 const HowItWorksPage: React.FC = () => {
   return (
     <div className="how-it-works-page">
@@ -26,22 +31,26 @@ const HowItWorksPage: React.FC = () => {
 
         <section className="how-it-works-steps">
           <div className="step">
-            <h2>Step 1: Sign Up</h2>
+            <h2 style={{ color: '#2E8B57' }}>Step 1: Sign Up</h2>
+            <img src={signup} alt="Signup Logo" id="signup-logo-howitworks" />
             <p>Register an account with Xplora to access personalized features.</p>
           </div>
 
           <div className="step">
-            <h2>Step 2: Explore</h2>
+            <h2 style={{ color: '#6666FF' }}>Step 2: Explore</h2>
+            <img src={explore} alt="Explore Logo" id="explore-logo-howitworks" />
             <p>Browse and explore the features available to you on the platform.</p>
           </div>
 
           <div className="step">
-            <h2>Step 3: Customize</h2>
+            <h2 style={{ color: '#FF9933' }}>Step 3: Customize</h2>
+            <img src={customize} alt="Customize Logo" id="customize-logo-howitworks" />
             <p>Personalize your experience by updating your settings and preferences.</p>
           </div>
 
           <div className="step">
-            <h2>Step 4: Connect</h2>
+            <h2 style={{ color: '#009999' }}>Step 4: Connect</h2>
+            <img src={connect} alt="Connect Logo" id="connect-logo-howitworks" />
             <p>Engage with the community and travel away.</p>
           </div>
         </section>
